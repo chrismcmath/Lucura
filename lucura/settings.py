797520681c9d3d5686e0ce5833640addba9d8184
Chris McMath <chrismcmath@dyn1212-238.wlan.ic.ac.uk>
2012-08-22 11:39:54 +0100

    Inc templates

--- lucura/settings.py
+++ lucura/settings.py
@@ -133,7 +133,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/chrismcmath/Documents/Django/templates"
+    "/Users/chrismcmath/Documents/Django/Lucura/templates"
 )
 
 INSTALLED_APPS = (
